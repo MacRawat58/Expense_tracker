98027dd0888e75eb6dbf60dd2d60187c36722cbf
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-06-08 13:52:58 +0530

    dropdown button added and its working fixed

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -12,6 +12,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -76,7 +77,31 @@ class _NewExpenseState extends State<NewExpense> {
                   ]))
             ],
           ),
+          const SizedBox(
+            height: 16,
+          ),
           Row(children: [
+            DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    {
+                      _selectedCategory = value;
+                    }
+                  });
+                }),
+            const Spacer(),
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
